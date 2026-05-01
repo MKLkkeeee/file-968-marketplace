e0feae6e2c8cd64f8e5bd4518367f8046e9a0c60
MKLkkeeee <dmwdfl@gmail.com>
2026-05-02 06:40:31 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -22,7 +22,7 @@ export const PLANARIA_API_KEY = "273bdfe95844615a5ae960a14907b2f5";
 export const TRUEWALLET_API = "https://www.planariashop.com/api/truewallet.php";
 export const CHECKSLIP_API = "https://www.planariashop.com/api/checkslip.php";
 // ชื่อผู้รับที่ยอมรับสำหรับสลิปธนาคาร
-export const BANK_RECEIVER_NAME = "ด.ช. ธวัชชัย คอทอง";
+export const BANK_RECEIVER_NAME = "ด.ช. ธวัชชัย ค";
 // เบอร์/พร้อมเพย์ของร้าน
 export const SHOP_TRUEWALLET_PHONE = "0832045174";
 export const SHOP_PROMPTPAY = "0832045174";
