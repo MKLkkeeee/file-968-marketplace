475f5a1101a60af55b57547bf2a0ce14fc046c0c
MKLkkeeee <dmwdfl@gmail.com>
2026-05-02 03:46:16 +0700

    Update Topup.tsx

--- src/pages/Topup.tsx
+++ src/pages/Topup.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import { get } from "firebase/database";
-import { ref } from "firebase/database";
+import { get, ref } from "firebase/database";
+import { db } from "@/lib/firebase";;
 import { Navbar } from "@/components/Navbar";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
